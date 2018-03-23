659a3749a75c874183d7c623a0428f879915b026
Abu Kalam Azad <37407607+AbuKalamAzad@users.noreply.github.com>
2018-03-23 16:38:29 +0530

    END OF Line

---
+++ SolutionStringEOF.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SolutionStringEOF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+        
+        int i=0;
+        while(sc.hasNext())
+        {
+        	 i++;
+        System.out.println(i+" "+sc.nextLine());
+       
+        }
+
+	}
+
+}
