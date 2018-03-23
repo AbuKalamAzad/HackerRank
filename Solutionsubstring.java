96db2257e5297033949109b3fced1f2233a2f2b3
Abu Kalam Azad <37407607+AbuKalamAzad@users.noreply.github.com>
2018-03-23 16:37:31 +0530

    Add files via upload

---
+++ Solutionsubstring.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Solutionsubstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+	    String str=scan.next();
+	    int k=scan.nextInt();
+	ArrayList a=new ArrayList();
+	
+	for(int i=0;i<=str.length()-k;i++)
+	{
+		a.add(str.substring(i, i+k));
+	}
+	System.out.println(a);
+	Collections.sort(a);
+	System.out.println(a);
+	System.out.println(a.get(0));
+	System.out.println(a.get(a.size()-1));
+	}
+	
+
+}
